5ef2ed77eb817d1ce5469c785a8ce59250ca6ac0
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-06-15 09:32:52 +0100

    [ADD] product_archive_changes

---
+++ product_archive_changes/__manifest__.py
@@ -0,0 +1,29 @@
+#! -*- encoding: utf-8 -*-
+{
+    'name': 'Changes to product archival',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'category': 'Extra Tools',
+    'summary': 'Changes to product archival',
+    'description': """
+
+- A group is created and only members from this group will be able to archive products
+    This is just about showing the 'archive' button and no changes are done to python code
+
+- Members of the NON-archive group will not be able to read archived products
+
+- Unarchive a BoM should unarchive all its components
+
+    """,
+    'author': 'Wibtec',
+    'website': 'http://www.wibtec.com',
+    'depends': ['mrp'],
+    'data': [
+            'security/groups.xml',
+            'security/ir_rule.xml',
+            'views/product_view.xml',
+            'views/mrp_view.xml',
+            ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ product_archive_changes/models/mrp.py
@@ -0,0 +1,27 @@
+#! -*- encoding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    @api.multi
+    def write(self, vals):
+        res = super(MrpBom, self.sudo()).write(vals)
+        if vals.get('active'):
+            for s in self:
+                # Unarchiving a BoM should mean unarchiving all its components
+                # Archiving a BoM SHOULD NOT mean archiving its components
+                for l in s.sudo().bom_line_ids:
+                    if not l.product_id.active:
+                        l.product_id.active = True
+        return res
+
+
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+
+    product_active = fields.Boolean('Active product', related='product_id.active')
