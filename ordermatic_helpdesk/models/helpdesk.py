d9efd06931c9edc54dc94accbbf450a358fa9145
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-06-26 08:06:51 +0100

    [UPD] ordermatic_helpdesk - filtering on fields

--- ordermatic_helpdesk/models/helpdesk.py
+++ ordermatic_helpdesk/models/helpdesk.py
@@ -8,4 +8,23 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     vendor_id = fields.Many2one('res.partner', 'Vendor', domain=[('supplier','=',True)])
+    product_id = fields.Many2one('product.product', 'Product', domain=[('sale_ok','=',True)])
     estimated_costs = fields.Float('Estimated costs')
+
+    @api.multi
+    def _get_team(self):
+        for s in self:
+            is_maintenance_team = False
+            is_product_change_team = False
+            if s.team_id:
+                if 'campus maintenance' in s.team_id.name.lower():
+                    is_maintenance_team = True
+                if 'product change' in s.team_id.name.lower():
+                    is_product_change_team = True
+            s.is_maintenance_team = is_maintenance_team
+            s.is_product_change_team = is_product_change_team
+
+    is_maintenance_team = fields.Boolean('Is maintenance team', compute='_get_team')
+    is_product_change_team = fields.Boolean('Is product change team', compute='_get_team')
+
+#OMC-68 - 07:30 >
