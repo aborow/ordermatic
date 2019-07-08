c5dc8366af0cd2f93b9bf45f2a0c36f4c5e71f40
Nupur Soni <nupur.soni@aktivsoftware.com>
2019-07-08 11:32:32 +0530

    [ADD]wibtec_sales

---
+++ wibtec_sales/models/sale_order.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+class SaleOrder(models.Model):
+
+	_inherit = "sale.order"
+
+	expected_install_date = fields.Date('Expected Install Date')
+	actual_install_date = fields.Date('Actual Install Date')
