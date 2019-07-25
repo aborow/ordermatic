a8e05510623087b6169792397bd6425a856895f5
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-07-25 22:16:01 +0100

    [ADD] ordermatic_tag_changes
    Only a certain group can create/edit/delete partner tags

---
+++ ordermatic_tag_changes/__manifest__.py
@@ -0,0 +1,26 @@
+#! -*- encoding: utf-8 -*-
+{
+    'name': 'Ordermatic - changes to permissions on tags',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'category': 'Extra Tools',
+    'summary': 'Changes to permissions on tags',
+    'description': """
+
+- creates a group to create partner tags
+- remmoves create/edit/delete permissions from existing groups
+
+    """,
+    'author': 'Wibtec',
+    'website': 'http://www.wibtec.com',
+    'depends': [
+                'contacts',
+                'sale',
+                'crm',
+                ],
+    'data': [
+            'security/security.xml',
+            ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
