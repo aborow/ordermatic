b5570d0b99c1e6298e6827d3f759e879fd1a54c3
Odoo Aktiv <nupur.soni@aktivsoftware.com>
2019-09-06 18:27:22 +0530

    [ADD]fields in product

--- wibtec_sales/__manifest__.py
+++ wibtec_sales/__manifest__.py
@@ -5,7 +5,7 @@
     'name': "Wibtec Sales",
 
     'summary': """
-        This Module is used to add dates in sale order from. """,
+        	This Module is used to add dates in sale order from. """,
 
     'description': """
         OMC-64 = BOM Custom Import
@@ -15,10 +15,11 @@
     'category': 'Sales',
     'version': '12.0.1.0.2',
     # any module necessary for this one to work correctly
-    'depends': ['sale'],
+    'depends': ['sale','delivery'],
     # always loaded
     'data': [
         'views/sale_order_view.xml',
+        'views/product_template_view.xml'
     ],
     'auto_install': False,
     'installable': True,
