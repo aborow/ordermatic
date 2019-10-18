0a8eb1bb7a1bcc117ea3b9e3679c30da629b063f
Rui Pedrosa Franco <rui.franco@gmail.com>
2019-10-18 11:02:18 +0100

    [FIX] ordermatic_mrp - missing field
    I was getting an error while installing ordermatic_mrp because of the location_dest_id not being set in product.product (just in product.template)

--- ordermatic_mrp/models/product.py
+++ ordermatic_mrp/models/product.py
@@ -9,3 +9,12 @@ class ProductTemplate(models.Model):
                                         'Finished Products Location',
                                         help='Default finished goods location'
                                         )
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    location_dest_id = fields.Many2one(
+                                        'stock.location',
+                                        'Finished Products Location',
+                                        help='Default finished goods location'
+                                        )
