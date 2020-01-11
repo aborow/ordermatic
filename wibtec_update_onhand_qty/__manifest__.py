da35a4cd9b06b2e4b193d7b94e2a522fd23d8ee7
Odoo Aktiv <nupur.soni@aktivsoftware.com>
2020-01-11 17:03:27 +0530

    [ADD]changes to update onhand qty

--- wibtec_update_onhand_qty/__manifest__.py
+++ wibtec_update_onhand_qty/__manifest__.py
@@ -7,6 +7,7 @@
 
     'description': """
         OMC-79 Update Onhand Quantity based on sheet.
+        OMC-192 Need to Change locations of products in Parallel
     """,
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -14,7 +15,7 @@
     'author': "WIB Technologies, Inc",
     'website': "http://www.wibtec.com",
     'category': 'product',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['product','stock'],
@@ -23,6 +24,7 @@
     'data': [
         'security/ir.model.access.csv',
         'wizard/update_qty_onhand_view.xml',
+        'wizard/update_locations_view.xml'
     ],
     'auto_install': False,
     'installable': True,
--- wibtec_update_onhand_qty/wizard/__init__.py
+++ wibtec_update_onhand_qty/wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import update_qty_onhand
+from . import update_locations
