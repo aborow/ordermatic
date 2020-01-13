7df715ed65d3d8333d56536f108223a7370f45fe
Virali <virali@aktivsoftware.com>
2020-01-13 22:52:36 +0530

    [ADD]changes for OMC-205 MRP II module keeps getting loaded issue and OMC-169

--- wibtec_mrp/__manifest__.py
+++ wibtec_mrp/__manifest__.py
@@ -10,20 +10,21 @@
     'description': """
         OMC-117 = Work order kanban card colors
         OMC-197 = Add Default Finished Good location to the Routing
+        OMC-169 = Update BOM view to show 150 records instead of 40 records
     """,
     'author': "WIB Technologies, Inc",
     'website': "http://www.wibtec.com",
     'category': 'Manufacturing',
-    'version': '12.0.1.0.4',
+    'version': '12.0.1.0.5',
     # any module necessary for this one to work correctly
     'depends': [
                 'mrp',
-                #'mrp_workorder'
                 ],
     # always loaded
     'data': [
         'views/mrp_workorder_view.xml',
-        'views/mrp_routing_view.xml'
+        'views/mrp_routing_view.xml',
+        'views/mrp_bom_view.xml'
     ],
     'auto_install': False,
     'installable': True,
