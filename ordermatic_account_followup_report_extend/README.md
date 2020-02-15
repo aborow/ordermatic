6f29e46a433672b8067d99a3c21c9606a64ef528
Odoo Aktiv <nupur.soni@aktivsoftware.com>
2020-02-15 15:54:15 +0530

    [ADD]OMC-235 Customer Statement Change

---
+++ ordermatic_account_followup_report_extend/README.md
@@ -0,0 +1,9 @@
+Open ERP System :- Odoo 12E Version 
+
+Installation 
+============
+Install the Application => Apps ->  (Technical Name: ordermatic_account_followup_report_extend)
+
+OMC-235
+====================
+-Customer Statement Change
---
+++ ordermatic_account_followup_report_extend/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full
+# copyright and licensing details.
+{
+    'name': "OrderMatic Account Followup Report",
+
+    'summary': """This Module is used to change the customer statement report. """,
+
+    'description': """
+        OMC-235 = "Customer Statement Change"
+    """,
+    'author': "WIB Technologies, Inc",
+    'website': "http://www.wibtec.com",
+    'category': 'Accounts',
+    'version': '12.0.1.0.0',
+    # any module necessary for this one to work correctly
+    'depends': ['account_reports'],
+    # always loaded
+    'data': [
+       
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+    # only loaded in demonstration mode
+}
