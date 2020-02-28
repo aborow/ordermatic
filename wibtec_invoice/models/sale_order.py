01da2bb635da0d0632f589eda3d0b7f41d81a42f
Odoo <odoo>
2020-02-28 23:13:10 +0530

    [ADD] CSOI-243: Change label Notes for Invoice in the SO view

--- wibtec_invoice/models/sale_order.py
+++ wibtec_invoice/models/sale_order.py
@@ -4,13 +4,13 @@ from odoo import api, fields, models, _
 
 
 class SaleOrder(models.Model):
-	_inherit = "sale.order"
+    _inherit = "sale.order"
 
-	note_of_invoice = fields.Text("Notes of Invoice")
+    note_of_invoice = fields.Text("Notes for Invoice")
 
-	@api.multi
-	def _prepare_invoice(self):
-		result = super(SaleOrder, self)._prepare_invoice()
-		if self.note_of_invoice:
-			result.update({'note_of_invoice': self.note_of_invoice})
-		return result
+    @api.multi
+    def _prepare_invoice(self):
+        result = super(SaleOrder, self)._prepare_invoice()
+        if self.note_of_invoice:
+            result.update({'note_of_invoice': self.note_of_invoice})
+        return result
