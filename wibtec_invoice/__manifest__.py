8c205d01b2aa79212183fb3bfd0d4bb382621d11
Nupur Soni <nupur.soni@aktivsoftware.com>
2020-05-04 19:37:52 +0530

    [ADD] OMC-267 Header is overlapping Terms and Conditions section

--- wibtec_invoice/__manifest__.py
+++ wibtec_invoice/__manifest__.py
@@ -22,11 +22,12 @@
         OMC-266 = Add Amount Due to Invoice Print Out
         OMC-275 = Tracking Number Issue while accesing Invoices
         OMC-245 = Setup Terms and Conditions for both Sales Orders and Invoices
+        OMC-267 = Header is overlapping Terms and Conditions section
     """,
     'author': "WIB Technologies, Inc",
     'website': "http://www.wibtec.com",
     'category': 'Invoice',
-    'version': '12.0.1.0.17',
+    'version': '12.0.1.0.18',
     # any module necessary for this one to work correctly
     'depends': ['account','l10n_us_check_printing', 'sale_management','sale'],
     # always loaded
