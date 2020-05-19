6b4a7d61d3b984bc608ce29be1fcd5f5f8f2b153
Nupur Soni <nupur.soni@aktivsoftware.com>
2020-05-19 13:19:51 +0530

    Merge OMC-282

---
+++ ordermatic_payment_applied_report/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full
+# copyright and licensing details.
+{
+    'name': "Payment Applied Report",
+
+    'summary': """
+        	This Module is used to export the record of payment into the proper format. """,
+
+    'description': """
+        OMC-282 : Payment Applied Report
+    """,
+    'author': "WIB Technologies, Inc",
+    'website': "http://www.wibtec.com",
+    'category': 'Sales',
+    'version': '12.0.1.0.4',
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/payment_applied_report_view.xml',
+        'views/account_payment_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+    # only loaded in demonstration mode
+}
