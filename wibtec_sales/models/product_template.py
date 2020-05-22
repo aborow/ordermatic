7d307bb4c81d58c0ae102bd56335956603fb22c9
Nupur Soni <nupur.soni@aktivsoftware.com>
2020-05-22 19:20:16 +0530

    [ADD] OMC-287 : Archiving Products

--- wibtec_sales/models/product_template.py
+++ wibtec_sales/models/product_template.py
@@ -8,3 +8,11 @@ class ProductTemplate(models.Model):
 	_inherit = "product.template"
 
 	long_desc = fields.Char('Long Description')
+
+	@api.multi
+	def write(self,vals):
+		res = super(ProductTemplate, self).write(vals)
+		if 'active' in vals and vals.get('active') == False:
+			if self.sale_ok == True:
+				self.update({'sale_ok':False})
+		return res
