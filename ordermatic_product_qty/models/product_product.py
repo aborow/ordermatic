064dad69696bf35ff41c43a372a199c05ac5bab4
odoo <odoo>
2020-12-09 22:55:28 +0530

    [FIX] OMC-352 fix the issue variable related

--- ordermatic_product_qty/models/product_product.py
+++ ordermatic_product_qty/models/product_product.py
@@ -16,22 +16,24 @@ class ProductProduct(models.Model):
         sale_order_lines = self.env['sale.order.line'].search([
             ('product_id', '=', self.id), ('state', '=', 'sale')
         ])
+        sale_order = False
         if sale_order_lines:
             sale_order = sale_order_lines.mapped('order_id')
-        action = self.env.ref(
-            'sale.action_orders').read()[0]
-        action["context"] = {"create": False}
-        if len(sale_order) > 1:
-            action['domain'] = [('id', 'in', sale_order.ids)]
-        elif len(sale_order) == 1:
-            form_view = [(self.env.ref('sale.view_order_form').id, 'form')]
-            if 'views' in action:
-                action['views'] = form_view + [
-                    (state, view)
-                    for state, view in action['views'] if view != 'form']
+        if sale_order:
+            action = self.env.ref(
+                'sale.action_orders').read()[0]
+            action["context"] = {"create": False}
+            if len(sale_order) > 1:
+                action['domain'] = [('id', 'in', sale_order.ids)]
+            elif len(sale_order) == 1:
+                form_view = [(self.env.ref('sale.view_order_form').id, 'form')]
+                if 'views' in action:
+                    action['views'] = form_view + [
+                        (state, view)
+                        for state, view in action['views'] if view != 'form']
+                else:
+                    action['views'] = form_view
+                action['res_id'] = sale_order.ids[0]
             else:
-                action['views'] = form_view
-            action['res_id'] = sale_order.ids[0]
-        else:
-            action = {'type': 'ir.actions.act_window_close'}
+                action = {'type': 'ir.actions.act_window_close'}
         return action
